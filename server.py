33ef0f9d516dccce1123b61afdea44c7d0a117ef
cup_of_mouse <94435720+cupofmouse@users.noreply.github.com>
2025-12-30 21:31:16 +0900

    Log base64 result image details in upload_image
    Added print statements to log the length and first 100 characters of the base64 encoded result image.

--- server.py
+++ server.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
-    result_path=None
+    result_img=None
     if request.method == 'POST':
         file = request.files['image']
         n_colors=int(request.form.get('n_colors', 5))
-        #request.form: POST방식의 html폼 데이터에 접근한다.
+
         in_memory_file=file.read()
         np_img=np.frombuffer(in_memory_file, np.uint8)
         img=cv2.imdecode(np_img, cv2.IMREAD_COLOR)
@@ -20,12 +20,13 @@ def upload_image():
         result_img=convert_img(img, n_colors)
         _,buffer=cv2.imencode('.png', result_img)
         result_base64=base64.b64encode(buffer).decode('utf-8')
-        
+        print("Result base64 length: ", len(result_base64))
+        print("Result img: ", result_base64[:100])
         return render_template('index.html', result_img=result_base64)
     return render_template('index.html')
-    #render_template는 html등 템플릿 파일을 서버에 렌더링하여 결과생성, 반환한다.
-    #templates폴더에 있는 html파일을 가져온다.
-    #두번째인자부턴 html에 넘겨줄 변수(데이터)를 전달한다.
 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
