c2f860cd26c6c863914099a2777987b8c4997a92
cup_of_mouse <94435720+cupofmouse@users.noreply.github.com>
2025-12-30 21:37:55 +0900

    Add main block to run Flask app
    Added a main block to run the Flask app on a specified port.

--- server.py
+++ server.py
@@ -25,4 +25,9 @@ def upload_image():
         return render_template('index.html', result_img=result_base64)
     return render_template('index.html')
 
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
